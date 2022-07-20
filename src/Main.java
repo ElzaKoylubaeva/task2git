788e0fae1b7cebdc2365f14dc0520faa0fbb3535
Elza Koylubaeva <zakirova_elza@mail.ru>
2022-07-20 14:26:42 +0500

    .gitignore, README.md, Main.java were added

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static final int SIZE = 30;
+
+    public static void main(String[] args) {
+
+        int[] sales = new int[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            sales[i] = (int) (Math.random() * 100);
+        }
+
+        SalesManager salesManager = new SalesManager(sales);
+        int max = salesManager.max();
+        System.out.println("The highest monthly sales " + max);
+    }
+}
+
